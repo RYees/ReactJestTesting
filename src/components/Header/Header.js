7198a0aefd2e0c0165ba1ed3f504be0906049505
Rehmet Yeshanew <ryfgmz87@gmail.com>
2022-10-13 16:35:27 -0400

    header testing

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Header.css"
+
+export default function Header({
+    title
+}) {
+    return(
+        <>
+         <h1 className="header" data-testid="header-1">{title}</h1>
+         <h2>FakeName</h2>
+         <h3 title="Header" className='header' >TestName</h3>
+        </>
+    )
+     
+}
